c81a3ff1e9d2378d2b95cc5e610193326f35c507
Jorge Hernández <lesthack@gmail.com>
2020-02-11 17:59:50 -0600

    :+: ¬¬

--- PruebaNivea/urls.py
+++ PruebaNivea/urls.py
@@ -5,11 +5,13 @@ from django.contrib.admin import site
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path
+from core.views import *
 
 admin.site.site_header = "Proyecto Nivea"
 admin.site.site_title = "Nivea"
 
 urlpatterns = [
     url(r'^$', RedirectView.as_view(url='/admin')),
+    url(r'^admin/core/aplicacion/nueva/$', site.admin_view(test)),
     path('admin/', admin.site.urls),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- core/views.py
+++ core/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
+from django.template import RequestContext
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
+from django.views.decorators.csrf import csrf_protect
+from django.core.exceptions import PermissionDenied
+from django.db import connection, transaction
+from django.utils.html import escape
 
-# Create your views here.
+def test(request):
+    return render(request, 'base_custom.html', {
+        'user': request.user
+    })
